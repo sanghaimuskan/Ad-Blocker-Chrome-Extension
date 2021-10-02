8f07d17189111f2459ea755ac787818729f35d64
Muskan Sanghai <43779438+sanghaimuskan@users.noreply.github.com>
2021-10-02 09:25:48 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,18 @@
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
+ 
